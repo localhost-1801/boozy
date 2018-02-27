e9830ae1a73b96dc6baf409cc6b093716c096c63
Warren Mui <warrenmui3225@gmail.com>
2018-02-27 14:46:59 -0600

    WIP - Added about and contact components

---
+++ client/components/about.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => (
+    <div>
+        <p>Cras sollicitudin congue turpis a rhoncus. Duis metus arcu,
+        maximus et sollicitudin eu, congue eget leo. Ut dignissim lacus
+        sed elit efficitur, in porttitor est accumsan. Praesent eget sem
+        vitae lectus lacinia tristique. Quisque id ex id ligula lobortis
+        efficitur. Ut gravida enim in dictum finibus. Vestibulum tincidunt
+        dolor ut ipsum hendrerit, vitae pulvinar dui dictum. Curabitur
+        condimentum volutpat ligula, sed gravida ligula. Cras feugiat orci
+        nec nunc dapibus, non ornare ante accumsan. Pellentesque accumsan
+        luctus odio nec sodales. Pellentesque at laoreet ante, id volutpat
+        enim. Integer a mollis justo, vel efficitur mauris. Donec consequat
+        diam in nibh tincidunt pretium.
+        </p>
+    </div>
+)
+export default About;
