f4b70033fbf560ebfe364b1388eb18f4b980d9cc
Warren Mui <warrenmui3225@gmail.com>
2018-02-27 15:13:11 -0600

    WIP - minor text edits

--- client/components/unauthuser-home.js
+++ client/components/unauthuser-home.js
@@ -4,7 +4,7 @@ const UnAuthUserHome = () => {
     return (
       <div>
         <h3>Welcome to our Boozy Vineyard and Winery!</h3>
-        <p>`Please feel free to check out our selection of the finest wines around!</p>
+        <p>Please feel free to check out our selection of the finest wines around!</p>
       </div>
     )
   }
