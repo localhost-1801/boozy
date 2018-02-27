36f2b4288ded005c8e9b7fa55490f83c3a690905
Warren Mui <warrenmui3225@gmail.com>
2018-02-27 15:53:44 -0600

    wip - added age gate to unAuthUser home page

--- client/components/unauthuser-home.js
+++ client/components/unauthuser-home.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const UnAuthUserHome = () => {
+export default class UnAuthUserHome extends Component {
+  constuctor(props) {
+    super(props)
+    this.state = {
+      over21: false
+    }
+  }
+  render() {
+    let homeOrAgeGate;
+    if (this.state.over21) {
+      homeOrAgeGate = (
+        <div>
+          <h3>Welcome to our Boozy Vineyard and Winery!</h3>
+          <p>Please feel free to check out our selection of the finest wines around!</p>
+        </div>
+      )
+    } else {
+      homeOrAgeGate = (
+        <div>
+          <button onClick={() => this.setState({ over21: true })}>
+            Click here if you are over 21
+          </button>
+        </div>
+      )
+    }
+
     return (
-      <div>
-        <h3>Welcome to our Boozy Vineyard and Winery!</h3>
-        <p>Please feel free to check out our selection of the finest wines around!</p>
-      </div>
+      { homeOrAgeGate }
     )
   }
-
-export default UnAuthUserHome;
+}
