ebacc1579b0491018e9aaaed6b37394db3e50e07
Warren Mui <warrenmui3225@gmail.com>
2018-02-27 15:04:43 -0600

    Added-newUser-home-page

---
+++ client/components/unauthuser-home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UnAuthUserHome = () => {
+    return (
+      <div>
+        <h3>Welcome to our Boozy Vineyard and Winery!</h3>
+        <p>Please feel free to check out our selection of the world's finest wines!</p>
+      </div>
+    )
+  }
+
+export default UnAuthUserHome;
