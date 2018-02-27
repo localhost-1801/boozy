95e229132699e9ed2d39775b50c8949f28673e6d
Warren Mui <warrenmui3225@gmail.com>
2018-02-27 10:07:39 -0600

    WIP - seeding DB, we have enough to build express routes

--- server/db/models/cartItem.js
+++ server/db/models/cartItem.js
@@ -2,5 +2,33 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const CartItem = db.define('cartItem', {
-
+    quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+            min: 1
+        }
+    }
+    // scopes: {
+    //     allProducts: {
+    //         include: [
+    //             {
+    //                 model: db.model('product'),
+    //                 through: {
+    //                     attributes: ['orderId', 'productId'],
+    //                     where: { orderId: this.id }
+    //                 }
+    //             }
+    //         ]
+    //     }
+    // },
+    // getterMethods: {
+    //     totalPrice() {
+    //         // return this.quantity 
+    //     }
+    // }
 })
+
+
+
+module.exports = CartItem;
