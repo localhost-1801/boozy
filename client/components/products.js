32c89ad749970363757dd30873d00bef447079a2
Charley Kirkpatrick <ckirkpat7@gmail.com>
2018-03-01 22:13:52 -0600

    wip filter by one category and search

--- client/components/products.js
+++ client/components/products.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { auth } from '../store'
-import { Card, Image, Icon, Button, Segment, Menu, Input } from 'semantic-ui-react'
+import { Card, Image, Icon, Button, Label, Segment, Menu, Input } from 'semantic-ui-react'
 import { fetchProducts } from '../store/products.js'
 import { Link } from 'react-router-dom'
 
@@ -11,38 +11,63 @@ import { Link } from 'react-router-dom'
 export class Products extends Component {
     constructor(props) {
         super(props)
-        this.state = { activeItem: 'All' }
+        this.state = {
+            activeItem: 'all',
+            input: '',
+        }
     }
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+    handleItemClick = (e, { id }) => this.setState({ activeItem: id })
+    handleInput = (e) => {
+        console.log(e.target.value)
+        this.setState({ input: e.target.value })
+    }
 
-    // componentDidMount() {
-    //     this.props.fetchProducts();
-    // }
+
     render() {
-        console.log(this.props.products)
         const { activeItem } = this.state
+        const filteredProducts = this.props.products.filter(wine => {
+            if (wine.title.toLowerCase().indexOf(this.state.input.toLowerCase()) > -1){
+                if (this.state.activeItem === 'all') {
+                    return true
+                } else {
+                    // if(wine.categories.find(category => {
+                    //     if(category.name === this.state.activeItem) return true
+                    // })) return true
+                    return wine.categories.find(category => {
+                        return category.name === this.state.activeItem
+                    })
+                }
+            } else {
+                return false
+            }
+        })
+        
         return (
             <div>
                 <Menu attached='top' >
-                    <Menu.Item name='All' active={activeItem === 'All'} onClick={this.handleItemClick} />
-                    <Menu.Item name='Reds' active={activeItem === 'Reds'} onClick={this.handleItemClick} />
-                    <Menu.Item name='Whites' active={activeItem === 'Whites'} onClick={this.handleItemClick} />
+                    <Menu.Item name='All' id='all' active={activeItem === 'all'} onClick={this.handleItemClick} />
+                    <Menu.Item name='Reds' id='red' active={activeItem === 'red'} onClick={this.handleItemClick} />
+                    <Menu.Item name='Whites' id='white' active={activeItem === 'white'} onClick={this.handleItemClick} />
                     <Menu.Menu position='right'>
                         <Menu.Item>
-                            <Input transparent icon={{ name: 'search', link: true }} placeholder='Search users...' />
+                            <Input onChange={this.handleInput} transparent icon={{ name: 'search', link: true }} placeholder='Search our wines...' />
                         </Menu.Item>
                     </Menu.Menu>
                 </Menu>
-                <Card.Group>
-                    {this.props.products.map(wine => {
+                <div style={{ height: '15px' }} />
+                <div style={{paddingLeft: '15rem', paddingRight:'15rem'}}>
+                <Card.Group itemsPerRow={3}>
+                    {/* {this.props.products.map(wine => { */}
+                    {filteredProducts.map(wine => {
+
                         return (
                             <Card
                                 link
                                 key={wine.id}
                                 centered={true}
                             >
-                                <Image src='img/red.jpg' />
-                                <Card.Content>
+                                <Image as={Link} to={`/products/${wine.id}`} src='https://files.slack.com/files-pri/T024FPYBQ-F9FGYBYEP/untitled-3.jpg' />
+                                <Card.Content as={Link} to={`/products/${wine.id}`}>
                                     <Card.Meta>
                                         <span className='date'>
                                             {wine.year}
@@ -60,14 +85,18 @@ export class Products extends Component {
                                 <Card.Content extra>
                                     <Segment.Group horizontal>
                                         <Segment>${wine.price}</Segment>
-                                        <Segment><Button as={Link} to={`/products/${wine.id}`} size={'small'}>VIEW</Button></Segment>
-                                        <Segment><Button  >ADD</Button></Segment>
+                                        <Segment>
+                                            <Label as={Link} to='/test'>
+                                                <Icon name="add to cart"/>
+                                            </Label>
+                                        </Segment>
                                     </Segment.Group>
                                 </Card.Content>
                             </Card>
                         )
                     })}
                 </Card.Group>
+                </div>
             </div>
         )
     }
