a2d4b83cf2a9c6fa02dc8574a8de3129f692fa21
Thomas Lawless <tlawless94@gmail.com>
2018-03-06 10:01:19 -0600

    Orders view populates with user generated data

--- client/components/orders.js
+++ client/components/orders.js
@@ -28,90 +28,40 @@ class Orders extends Component {
       return(
         <p> Must Login to see previous orders </p>
       )
-    } else {
+    }
     return (
       <div>
         <div className='cart'>
         </div>
-        {this.props.orders.map( item => (
-          <div>
-            Cart Id {item.cartId}
-          </div>
-        ))}
 
         <Header as='h2' color='black' textAlign='center'>
         {' '}Your Orders
-    </Header>
-      <div className="ordersTitle">
-        <Table collapsing fixed color='olive' key='olive'>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell colSpan='5'>Placed order on February 26, 2018</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-
-          <Table.Body>
-            <Table.Row textAlign='center'>
-              <Table.Cell><Image src='img/white.jpg' size='medium'/></Table.Cell>
-              <Table.Cell><b>Chardonnay</b></Table.Cell>
-            <Table.Cell><b>Subtotal:</b> <br />$20.00</Table.Cell>
-              <Table.Cell><b>Status:</b> <br />Shipped</Table.Cell>
-              <Table.Cell>
-                <a
-                  target='_self'
-                  href='#'>
-                  <Button color='olive'>Write review</Button>
-                </a>
-              </Table.Cell>
-            </Table.Row>
-          </Table.Body>
-
-          <Table.Body>
-          <Table.Row textAlign='center'>
-            <Table.Cell><Image src='img/red.jpg' size='medium'/></Table.Cell>
-            <Table.Cell><b>Merlot</b></Table.Cell>
-          <Table.Cell><b>Subtotal:</b> <br />$10.00</Table.Cell>
-            <Table.Cell><b>Status:</b> <br />Shipped</Table.Cell>
-            <Table.Cell>
-              <a
-                target='_self'
-                href='#'>
-                <Button color='olive'>Write review</Button>
-              </a>
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-        </Table>
-
-
-
-        <Table collapsing fixed color='olive' key='olive'>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell colSpan='5'>Placed order on February 28, 2018</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          <Table.Row textAlign='center'>
-            <Table.Cell><Image src='img/white.jpg' size='medium'/></Table.Cell>
-            <Table.Cell><b>Chardonnay</b></Table.Cell>
-          <Table.Cell><b>Subtotal:</b> <br />$20.00</Table.Cell>
-            <Table.Cell><b>Status:</b> <br />Shipped</Table.Cell>
-            <Table.Cell>
-              <a
-                target='_self'
-                href='#'>
-                <Button color='olive'>Write review</Button>
-              </a>
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
-      </div>
-      </div>
+        </Header>
+        {this.props.orders.map( item => (
+          <div key={item.id}>
+            <div className="ordersTitle">
+              <Table collapsing fixed color='olive' key='olive'>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell colSpan='5'>Placed order</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                {item.products.map( product => (
+                  <Table.Body key={product.id}>
+                    <Table.Row textAlign='center'>
+                      <Table.Cell><Image src='img/white.jpg' size='medium'/></Table.Cell>
+                      <Table.Cell><b>{product.title}</b></Table.Cell>
+                    <Table.Cell><b>Subtotal:</b> <br />{product.cartItem.purchasePrice * product.cartItem.quantity}</Table.Cell>
+                      <Table.Cell><b>Status:</b> <br />{item.status}</Table.Cell>
+                    </Table.Row>
+                  </Table.Body>
+                ))}
+              </Table>
+              </div>
+            </div>
+        ))}
+        </div>
     )
-    }
   }
 }
 
