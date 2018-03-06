48c0e47ff15b696eb23b3374d710b739ad647575
Nirali Shah <hellonirali@Niralis-MacBook-Air.local>
2018-03-06 16:56:45 -0600

    updated about page

--- client/components/about.js
+++ client/components/about.js
@@ -18,22 +18,18 @@ const About = () => (
                 <Grid.Column>
                     <Image src='/img/team/charlie.png' size="medium" />
                     <div className="about-name">Charley Kirkpatrick </div>
-                    <p className="title">Chief Llama Officer (CLO)</p>
                 </Grid.Column>
                 <Grid.Column>
                     <Image src='/img/team/nirali.png' size="medium" />
                     <div className="about-name">Nirali Shah</div>
-                    <p className="title">Chief Aesthetics Officer (CAO)</p>
                 </Grid.Column>
                 <Grid.Column>
                     <Image src='/img/team/thomas.png' size="medium" />
                     <div className="about-name">Thomas Lawless</div>
-                    <p className="title">Chief Something (CS)</p>
                 </Grid.Column>
                 <Grid.Column>
                     <Image src='/img/team/warren.png' size="medium" />
                     <div className="about-name">Warren Mui</div>
-                    <p className="title">Cheif Something (CS)</p>
                 </Grid.Column>
             </Grid.Row>
         </Grid>
