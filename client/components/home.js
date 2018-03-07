1f4b1da2bbd1e975edb3cad3c4786f3af866e857
Charley Kirkpatrick <ckirkpat7@gmail.com>
2018-03-06 20:01:53 -0600

    beep boop

--- client/components/home.js
+++ client/components/home.js
@@ -1,12 +1,17 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import {Container, Header} from 'semantic-ui-react'
 import {
   Button,
   Icon,
 } from 'semantic-ui-react'
 
 const Home = () => (
-  <div className="indexBackground"></div>
+  <div className="indexBackground">
+    <Container>
+    <Header inverted as='h2'>The best wines are the ones we drink with friends</Header>
+    </Container>
+  </div>
 )
 
 export default Home
